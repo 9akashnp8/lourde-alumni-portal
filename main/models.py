3cd1f8dbc0805dd6f8f58fbfa7ec326d8aa64e7c
Akash NP <akashnp1998@gmail.com>
2022-05-31 21:23:54 +0530

    Create auto generating Alumni Number field that populates based on id of registeree

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Alumni(models.Model):
-    id = models.AutoField(primary_key=True)
+    alumni_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField()
     batch = models.CharField(max_length=100)
@@ -10,3 +10,12 @@ class Alumni(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        super(Alumni, self).save(*args, **kwargs)
+        if not self.alumni_no:
+            prefix = 'LMS-ALUM-'
+            id = self.alumni_id
+            self.alumni_no = prefix+str(id)
+            super(Alumni, self).save(*args, **kwargs)
+        
