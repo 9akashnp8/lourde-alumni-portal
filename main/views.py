95880d7adf98c778e83dec890fafabd0354d50b5
Akash NP <akashnp1998@gmail.com>
2022-06-02 05:56:53 +0530

    Configure project & app to send email

--- main/views.py
+++ main/views.py
@@ -1,8 +1,14 @@
 import razorpay
 from django.shortcuts import redirect, render
+from django.core.mail import send_mail
 from .forms import RegistrationForm
 
 client = razorpay.Client(auth=("rzp_live_lf0bv0XHtSh7so","RNzB8Brxh6WAL7Rv2HAvNfH8"))
+
+#Confirmation Email
+SUBJECT = f"Welcome to the family {name}! You have successfully registered as an LMS Alumni"
+MESSAGE = "Hello {name}, thank you for being a part of our family."
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -17,24 +23,6 @@ def register(request):
             email = form.cleaned_data["email"]
             payment_url = "https://pages.razorpay.com/pl_JbXTzuuJU9gcsA/view?"
             return redirect(f"{payment_url}email={email}&name={name}")
-            # r = client.payment_link.create({
-            #     "amount": 100,
-            #     "currency": "INR",
-            #     "accept_partial": False,
-            #     "customer": {
-            #         "name": form.cleaned_data["name"],
-            #         "email": form.cleaned_data["email"],
-            #         "contact": "+918075680338"
-            #     },
-            #     "notify": {
-            #         "sms": True,
-            #         "email": False,
-            #     },
-            #     "callback_url": "http://127.0.0.1",
-            #     "callback_method": "get"
-            # })
-            # print(r)
-            # return redirect(r["short_url"])
 
     context = {'form':form}    
     return render(request, 'register.html', context=context)
