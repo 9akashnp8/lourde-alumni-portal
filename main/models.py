821db231f8e86b147c8e31a1b6ada083c5607313
Akash NP <akashnp1998@gmail.com>
2022-06-07 22:34:54 +0530

    Update Alumni model to include phone, modify field name from alumni_no to application_no

--- main/models.py
+++ main/models.py
@@ -2,20 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Alumni(models.Model):
-    alumni_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    phone = models.IntegerField()
     email = models.EmailField()
     batch = models.CharField(max_length=100)
-    alumni_no = models.CharField(max_length=100)
+    application_no = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
     
     def save(self, *args, **kwargs):
         super(Alumni, self).save(*args, **kwargs)
-        if not self.alumni_no:
-            prefix = 'LMS-ALUM-'
-            id = self.alumni_id
-            self.alumni_no = prefix+str(id)
+        if not self.application_no:
+            prefix = 'LMS-APPL-'
+            id = self.id
+            self.application_no = prefix+str(id)
             super(Alumni, self).save(*args, **kwargs)
         
