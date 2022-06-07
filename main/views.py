7cd02fccc0b1a15c7a562b12ba60d0fc88f96590
Akash NP <akashnp1998@gmail.com>
2022-06-07 22:30:35 +0530

    Modify register form to NOT save, instead save submitted data to django sessions for redirection to verification page

--- main/views.py
+++ main/views.py
@@ -6,23 +6,27 @@ from django.core.mail import send_mail
 from .forms import RegistrationForm
 
 #Confirmation Email
-SUBJECT = "Welcome to the family! You have successfully registered as an LMS Alumni"
-MESSAGE = "Hello {name}, thank you for being a part of our family."
+SUBJECT = 'Welcome to the family! You have successfully registered as an LMS Alumni'
+MESSAGE = 'Hello {name}, thank you for being a part of our family.'
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def register(request):
-    form = RegistrationForm
-    if request.method == "POST":
+    form = RegistrationForm()
+    if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            name = form.cleaned_data["name"]
-            email = form.cleaned_data["email"]
-            payment_url = "https://pages.razorpay.com/pl_JbXTzuuJU9gcsA/view?"
-            return redirect(f"{payment_url}email={email}&name={name}")
+            request.session['registered_data'] = {
+                'name': form.cleaned_data['name'],
+                'email': form.cleaned_data['email'],
+                'phone': form.cleaned_data['phone'],
+                'batch': form.cleaned_data['batch'],
+            }
+            return HttpResponse("Registered")
+            # payment_url = 'https://pages.razorpay.com/pl_JbXTzuuJU9gcsA/view?'
+            # return redirect(f'{payment_url}email={email}&name={name}')
 
     context = {'form':form}    
     return render(request, 'register.html', context=context)
@@ -30,4 +34,4 @@ def register(request):
 @csrf_exempt
 def webhook(request):
 
-    return HttpResponse("200")
+    return HttpResponse('200')
